c36f0f66245dc97e0042953b8657ebbaaf140366
Majinbao1997 <2428732826@qq.com>
2021-01-06 18:58:37 +0800

    增加了图片/视频上传的原子接口

---
+++ yzl-product-server/src/main/java/cn/net/yzl/product/dao/ImageMapper.java
@@ -0,0 +1,18 @@
+package cn.net.yzl.product.dao;
+
+
+import cn.net.yzl.product.model.db.Image;
+
+public interface ImageMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Image record);
+
+    int insertSelective(Image record);
+
+    Image selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Image record);
+
+    int updateByPrimaryKey(Image record);
+}
