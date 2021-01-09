2723c1f3cb8b64a200c55d1f4fffc8ef8f1dace2
Majinbao1997 <2428732826@qq.com>
2021-01-09 09:42:26 +0800

    删除了图片库排序功能和创建人自动校验非空

--- yzl-product-common/src/main/java/cn/net/yzl/product/model/vo/imageStore/ImageStoreVO.java
+++ yzl-product-common/src/main/java/cn/net/yzl/product/model/vo/imageStore/ImageStoreVO.java
@@ -16,12 +16,9 @@ public class ImageStoreVO {
     private String imageUrl;
     @ApiModelProperty("描述")
     private String descri;
-    @ApiModelProperty("排序")
-    private Integer sort;
     @ApiModelProperty("类型（0：图片库，1：视频库）")
     @NotEmpty(message = "类型不能为空！")
     private Byte type;
-    @NotEmpty(message = "创建人不能为空！")
     private String createNo;
     @ApiModelProperty("图片库名称")
     @NotNull(message = "图片库名称不能为空！")
