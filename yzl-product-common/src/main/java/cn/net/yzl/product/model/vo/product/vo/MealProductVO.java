5322aaf430225d89a8d8727cb3d681b277051079
wanghuasheng <wanghuasheng@yuzhilin.net.cn>
2021-01-12 09:31:59 +0800

    添加接口必填字段描述

--- yzl-product-common/src/main/java/cn/net/yzl/product/model/vo/product/vo/MealProductVO.java
+++ yzl-product-common/src/main/java/cn/net/yzl/product/model/vo/product/vo/MealProductVO.java
@@ -14,13 +14,13 @@ public class MealProductVO {
     @ApiModelProperty(value = "套餐编码",name = "mealNo")
     private String mealNo;
 
-    @ApiModelProperty(value = "商品编码",name = "productCode")
+    @ApiModelProperty(value = "商品编码",name = "productCode" ,required = true)
     private String productCode;
 
-    @ApiModelProperty(value = "商品数量",name = "productNum")
+    @ApiModelProperty(value = "商品数量",name = "productNum",required = true)
     private Integer productNum;
 
-    @ApiModelProperty(value = "是否赠品(0代表否,1代表是)",name = "mealGiftFlag")
+    @ApiModelProperty(value = "是否赠品(0代表否,1代表是)",name = "mealGiftFlag",required = true)
     private Integer mealGiftFlag;
 
     @ApiModelProperty(value = "创建时间",name = "createTime")
