f930f783c67696182d8d8df48e7efe57b7b97644
lichanghong <lichanghong@yuzhilin.net.cn>
2021-01-12 09:50:34 +0800

    商品扣减、增加库存

---
+++ yzl-product-server/src/main/java/cn/net/yzl/product/config/Authentication.java
@@ -0,0 +1,11 @@
+package cn.net.yzl.product.config;
+
+/**
+ * @author lichanghong
+ * @version 1.0
+ * @title: Authentication
+ * @description todo
+ * @date: 2021/1/12 9:23 上午
+ */
+public class Authentication {
+}
